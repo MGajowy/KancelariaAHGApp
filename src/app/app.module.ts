18612e58531f29a8a631a38d2f094565e9072e1f
Michal <ga>
2020-06-30 08:16:43 +0200

    Add routing cd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,19 @@
+import { DefaultModule } from './layouts/default/default.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DefaultComponent } from './layouts/default/default.component';
-import { HomeComponent } from './modules/home/home.component';
-import { AboutComponent } from './modules/about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DefaultComponent,
-    HomeComponent,
-    AboutComponent
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    DefaultModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/layouts/default/default.module.ts
+++ src/app/layouts/default/default.module.ts
@@ -1,12 +1,22 @@
+import { RouterModule } from '@angular/router';
+import { AboutComponent } from './../../modules/about/about.component';
+import { HomeComponent } from './../../modules/home/home.component';
+import { DefaultComponent } from './default.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import {FlexLayoutModule} from '@angular/flex-layout';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    DefaultComponent,
+    HomeComponent,
+    AboutComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule,
+    FlexLayoutModule
   ]
 })
 export class DefaultModule { }
