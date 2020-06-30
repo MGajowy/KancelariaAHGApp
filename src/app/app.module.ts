a0b78fe06f1d06f0e64caba7042b5d692b67f17e
Michal <ga>
2020-06-30 07:33:04 +0200

    Add layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DefaultComponent } from './layouts/default/default.component';
+import { HomeComponent } from './modules/home/home.component';
+import { AboutComponent } from './modules/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DefaultComponent,
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
