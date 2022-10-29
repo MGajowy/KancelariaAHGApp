0b4e0645672a53ea865dca753339481d4c65ada8
michal.gajewski <michalpiotr.gajewski@gmail.com>
2022-10-29 13:02:19 +0200

    Changes to the modify category view

--- src/app/modules/regulation/component/modify-regulation-category/modify-regulation-category.component.ts
+++ src/app/modules/regulation/component/modify-regulation-category/modify-regulation-category.component.ts
@@ -1,29 +1,41 @@
 import { Component, OnInit } from '@angular/core';
-import { RegulationService} from '../../service/regulation.service';
+import { RegulationService } from '../../service/regulation.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { CategoryDTOrequest } from 'src/app/generated/REST';
+
+interface Choise {
+  value: string;
+  viewValue: string;
+}
 @Component({
   selector: 'app-modify-regulation-category',
   templateUrl: './modify-regulation-category.component.html',
   styleUrls: ['./modify-regulation-category.component.scss']
 })
+
 export class ModifyRegulationCategoryComponent implements OnInit {
   id: number;
   submitted = false;
   category: CategoryDTOrequest;
-
-  constructor(private router: Router, private regulationService: RegulationService, private route: ActivatedRoute) { }
+  list: Choise[] = [
+    {value: 'true', viewValue: 'TAK'},
+    {value: 'false', viewValue: 'NIE'},
+  ]
+  constructor(
+    private router: Router,
+    private regulationService: RegulationService,
+    private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.category = new CategoryDTOrequest();
     this.id = this.route.snapshot.params['id'];
     this.regulationService.detailsCategory(this.id)
-    .subscribe(data => {
-      this.category = data;
-    }, error => console.log(error));
+      .subscribe(res => {
+        this.category = res;
+      }, error => console.log(error));
   }
 
-  gotoList(){
+  gotoList() {
     this.reloadData();
     this.router.navigate(['/office/category-regulations']);
   }
@@ -34,14 +46,15 @@ export class ModifyRegulationCategoryComponent implements OnInit {
 
   updateCategory() {
     this.regulationService.updateCategory(this.id, this.category)
-      .subscribe(data => console.log(data), error => console.log(error));
+      .subscribe(data => console.log(data),
+        error => console.log(error));
     this.category = new CategoryDTOrequest();
 
     this.gotoList();
-}
+  }
 
-  reloadData(){
-  this.regulationService.getRegulationCategoryList();
-}
+  reloadData() {
+    this.regulationService.getRegulationCategoryList();
+  }
 
 }
--- src/app/modules/resolution/component/modify-category/modify-category.component.ts
+++ src/app/modules/resolution/component/modify-category/modify-category.component.ts
@@ -3,15 +3,25 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { CategoryDTOrequest } from 'src/app/generated/REST';
 
+
+interface Choise {
+  value: string;
+  viewValue: string;
+}
 @Component({
   selector: 'app-modify-category',
   templateUrl: './modify-category.component.html',
   styleUrls: ['./modify-category.component.scss']
 })
+
 export class ModifyCategoryComponent implements OnInit {
   id: number;
   submitted = false;
   category: CategoryDTOrequest;
+  list: Choise[] = [
+    {value: 'true', viewValue: 'TAK'},
+    {value: 'false', viewValue: 'NIE'},
+  ]
 
   constructor(private router: Router, private resolutionSevice: ResolutionService, private route: ActivatedRoute) { }
 
